eda40a45f666b0d3eb1eeffd14e100376d3bb427
Andrey Bogachevskiy <a.bogachevskiy@efko.ru>
2020-03-13 12:30:22 +0300

    removed source maps

--- webpack.config.js
+++ webpack.config.js
@@ -84,8 +84,6 @@ module.exports = {
 }
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports.devtool = '#source-map';
-
     module.exports.plugins = (module.exports.plugins || []).concat([
         new webpack.DefinePlugin({
             'process.env': {
